54f3cf2ab14975e565aaf189c72faafcc7fcbfec
gi003 <diraheta@ucsc.edu>
2025-03-08 17:11:04 -0800

    Adding implementation of Gallery closer to what I have in figma

--- gallery/js/art.js
+++ gallery/js/art.js
@@ -1,4 +1,4 @@
-
+//Making of visible divs------------------------------------
 let elements = document.getElementsByClassName("vis");
 console.log(elements);
 console.log(elements[0].style.backgroundColor)
@@ -9,15 +9,22 @@ for (let x of elements){
     b = Math.random() * 255;
     x.style.backgroundColor = `rgb(${r},${g},${b})`;
 }
-//Getting array 
+
+//Scrollable Gallery---------------------------------------
 let array = document.getElementById("array");
 let position = 0;
-//Scroll Event listenter 
 
-//n-1 * 100vw
+// Start at 0-100 and move negatively
+// +------+------------+
+// |      |      |      |
+// |      |      |      |
+// |      |      |      |
+// +------+------+------+
+// 0      100    200    300
 artworks_num = 4
 maxarray_pos = -((artworks_num - 1) * 100)
 
+//Key Events
 document.addEventListener('keydown',function(event) {
     let key = event.key;
     if (position < 0 && (key=='ArrowLeft' || key=='a')){
@@ -30,5 +37,50 @@ document.addEventListener('keydown',function(event) {
     }
 })
 
+//Mouse Events
+
+function toggleFullScreen(element) {
+
+    if (!document.fullscreenElement) {
+        // Enter fullscreen mode
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if (element.webkitRequestFullscreen) { /* Safari */
+            element.webkitRequestFullscreen();
+        } else if (element.msRequestFullscreen) { /* IE11 */
+            element.msRequestFullscreen();
+        }
+    } else {
+        // Exit fullscreen mode
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) { /* Safari */
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* IE11 */
+            document.msExitFullscreen();
+        }
+    }
+    }
+
 //Click Event Listener --- class name returns an HTML collection - multiple elements
-document.getElementsByClassName("image_container").addEventListener("click, displayDate");
+img_nodelist = document.querySelectorAll("img");
+console.log(img_nodelist)
+
+img_nodelist.forEach((img) => {
+    let text = img.parentNode.nextElementSibling;
+    let img_container = img.parentNode
+    let isExpanded = false;
+
+    img.addEventListener('click', () => {
+        if(!isExpanded) {
+            img_container.style.width = '40%'
+            img_container.style.left = '25vw'
+            text.style.visibility = 'visible'
+            isExpanded = true;
+        } else{
+            console.log('HELLOW WORLD')
+            toggleFullScreen(img);
+        }
+    })
+})
+
